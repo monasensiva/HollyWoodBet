573b6be233c9a66c263365e0ddccbfdb71027f44
monasensiva <72541854+monasensiva@users.noreply.github.com>
2020-10-20 11:50:51 +0200

    Upload

--- HWBUi/Controllers/EventController.cs
+++ HWBUi/Controllers/EventController.cs
@@ -1,17 +1,62 @@
-﻿using System;
+﻿using HWBUi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace HWBUi.Controllers
 {
     public class EventController : Controller
     {
-        // GET: Event
+        // GET: Tournament
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<UiEvent> uiEvents;
+            HttpResponseMessage response = GlobalVariables.webApiClient.GetAsync("Event").Result;
+            uiEvents = response.Content.ReadAsAsync<IEnumerable<UiEvent>>().Result;
+            return View(uiEvents);
+        }
+
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new UiEvent());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webApiClient.GetAsync("Event/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<UiEvent>().Result);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(UiEvent uiEvents)
+        {
+            if (uiEvents.EventId == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.webApiClient.PostAsJsonAsync("Event", uiEvents).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webApiClient.PutAsJsonAsync("Event/" + uiEvents.EventId
+                    , uiEvents).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.webApiClient.DeleteAsync("Event/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+
         }
     }
 }
--- HWBUi/Controllers/EventDetailController.cs
+++ HWBUi/Controllers/EventDetailController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HWBUi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,52 @@ namespace HWBUi.Controllers
 {
     public class EventDetailController : Controller
     {
-        // GET: EventDetail
+        // GET: Tournament
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<UiEventDetail> uiEventDetails;
+            HttpResponseMessage response = GlobalVariables.webApiClient.GetAsync("EventdDetail").Result;
+            uiEventDetails = response.Content.ReadAsAsync<IEnumerable<UiEventDetail>>().Result;
+            return View(uiEventDetails);
+        }
+
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new UiEventDetail());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webApiClient.GetAsync("EventdDetail/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<UiEventDetail>().Result);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(UiEventDetail uiEventDetails)
+        {
+            if (uiEventDetails.EventId == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.webApiClient.PostAsJsonAsync("EventdDetail", uiEventDetails).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webApiClient.PutAsJsonAsync("EventdDetail/" + uiEventDetails.EventDetailId
+                    , uiEventDetails).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.webApiClient.DeleteAsync("EventdDetail/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+
         }
     }
 }
